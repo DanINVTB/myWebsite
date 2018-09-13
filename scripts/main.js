3d81593ed5b3ed9881801b20ace5c390ec01ddb7
Daniel Huri <danielhuri.invtb@gmail.com>
2018-09-13 23:03:43 +0300

    Image changer
    Added image change when clicked

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
 
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc == 'https://png.pngtree.com/element_origin_min_pic/16/11/10/f373183c6a9b0cf797b9116f108f9978.jpg') {
+        myImage.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp6u7W1Kw8KqjkfuLQ_xtCwo25iUeYeEpWlOit5FgRpj13dLEx');
+    } else {
+        myImage.setAttribute('src', 'https://png.pngtree.com/element_origin_min_pic/16/11/10/f373183c6a9b0cf797b9116f108f9978.jpg');
+    }
+}
